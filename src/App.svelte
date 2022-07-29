49060957b3dba2407dab09bede9c138ace5185a2
M.D. Walters <808426@pdsb.net>
2022-07-29 18:21:56 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,37 @@
 <main>
-  e
+<div class="container-fluid p-5 bg-primary text-white">
+  <center><h1 class="display-1">M.D. Walters' Website!</h1></center>
+  <marquee class="h4">Look out for a Svelte rewrite of this website near you!</marquee>
+</div>
+<nav class="navbar navbar-expand-sm bg-light navbar-light shadow-sm">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/Mwalters75"><i class="bi bi-github"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/MDWalters124"><i class="bi bi-twitter"></i></a>
+       </li>
+       <li class="nav-item">
+        <a class="nav-link" href="https://deviantart.com/mdwalters"><i class="bi bi-brush-fill"></i></a>
+       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://imood.com/users/pancake"><i class="bi bi-emoji-smile-fill"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://mdwalterscreations.wordpress.com/author/mdwalters124/"><i class="bi bi-wordpress"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://reddit.com/user/pancakeflipper124/"><i class="bi bi-reddit"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://youtube.com/channel/UCnAOoxdT4-oCuESfNHK3HXQ"><i class="bi bi-youtube"></i></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="main_content">
+  <h2>About</h2>
+  <p>Hi, I'm M.D.! You may know me as MDWalters, MDWalters124, pancake, or pancake_flipper, but yep, they're all me. Most of the stuff I make are useless, but hey, people actually like them!</p>
+</div>
 </main>
