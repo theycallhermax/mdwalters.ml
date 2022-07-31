622caed73cb06657d5e8893eae618c5e52abfee9
M.D. Walters <808426@pdsb.net>
2022-07-30 23:00:27 -0400

    Add titles, for the clueless user

--- src/App.svelte
+++ src/App.svelte
@@ -10,25 +10,25 @@
   <div class="container-fluid">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="https://github.com/Mwalters75"><i class="bi bi-github"></i></a>
+        <a class="nav-link" href="https://github.com/Mwalters75"><i class="bi bi-github" title="GitHub"></i></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="https://twitter.com/MDWalters124"><i class="bi bi-twitter"></i></a>
+        <a class="nav-link" href="https://twitter.com/MDWalters124"><i class="bi bi-twitter" title="Twitter"></i></a>
        </li>
        <li class="nav-item">
-        <a class="nav-link" href="https://deviantart.com/mdwalters"><i class="bi bi-brush-fill"></i></a>
+        <a class="nav-link" href="https://deviantart.com/mdwalters"><i class="bi bi-brush-fill" title="DeviantArt"></i></a>
        </li>
       <li class="nav-item">
-        <a class="nav-link" href="https://imood.com/users/pancake"><i class="bi bi-emoji-smile-fill"></i></a>
+        <a class="nav-link" href="https://imood.com/users/pancake"><i class="bi bi-emoji-smile-fill" title="Imood"></i></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="https://mdwalterscreations.wordpress.com/author/mdwalters124/"><i class="bi bi-wordpress"></i></a>
+        <a class="nav-link" href="https://mdwalterscreations.wordpress.com/author/mdwalters124/"><i class="bi bi-wordpress" title="Blog"></i></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="https://reddit.com/user/pancakeflipper124/"><i class="bi bi-reddit"></i></a>
+        <a class="nav-link" href="https://reddit.com/user/pancakeflipper124/"><i class="bi bi-reddit" title="Reddit"></i></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="https://youtube.com/channel/UCnAOoxdT4-oCuESfNHK3HXQ"><i class="bi bi-youtube"></i></a>
+        <a class="nav-link" href="https://youtube.com/channel/UCnAOoxdT4-oCuESfNHK3HXQ"><i class="bi bi-youtube" title="YouTube"></i></a>
       </li>
     </ul>
   </div>
