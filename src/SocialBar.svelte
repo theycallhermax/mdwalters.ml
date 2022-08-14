9548c0b89060b5f8afee8eca7e293a953480f6e2
M.D. Walters <808426@pdsb.net>
2022-08-14 01:19:26 -0400

    Create SocialBar.svelte

---
+++ src/SocialBar.svelte
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-sm bg-light navbar-light shadow-sm text-wrap fs-5">
+   <div class="container-fluid">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/Mwalters75"><i class="bi bi-github" title="GitHub"></i></a>
+        </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://twitter.com/MDWalters124"><i class="bi bi-twitter" title="Twitter"></i></a>
+        </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://deviantart.com/mdwalters"><i class="bi bi-brush-fill" title="DeviantArt"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://imood.com/users/pancake"><i class="bi bi-emoji-smile-fill" title="Imood"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://mdwalterscreations.wordpress.com/author/mdwalters124/"><i class="bi bi-wordpress" title="Blog"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://reddit.com/user/pancakeflipper124/"><i class="bi bi-reddit" title="Reddit"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://youtube.com/channel/UCnAOoxdT4-oCuESfNHK3HXQ"><i class="bi bi-youtube" title="YouTube"></i></a>
+      </li>
+    </ul>
+  </div>
+</nav>
