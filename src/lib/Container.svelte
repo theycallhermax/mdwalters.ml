738aac648d217670484d5841b1279c27cf69811e
M.D. Walters <808426@pdsb.net>
2022-08-14 17:11:39 -0400

    Create Container.svelte

---
+++ src/lib/Container.svelte
@@ -0,0 +1,14 @@
+<script>
+  export var background;
+  export var colour;
+</script>
+
+<style>
+  slot {
+    background: {background};
+    color: {colour};
+    border-radius: 5px;
+  }
+</style>    
+
+</slot>
