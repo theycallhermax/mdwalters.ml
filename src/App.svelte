4d4f2e93b713c78046e539526c1c9d0a774f6ae0
M.D. Walters <808426@pdsb.net>
2022-08-14 00:33:27 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,13 @@
 <script>
+  import Header from "Header.svelte";
+  import SocialBar from "SocialBar.svelte";
+
   export var motd = "At school";
 </script>
 
 <main>
-  <div class="container-fluid p-5 bg-primary text-white">
-    <center><h1 class="display-1">M.D. Walters' Website!</h1></center>
-    <marquee class="h4">{motd}</marquee>
-  </div>
-  <nav class="navbar navbar-expand-sm bg-light navbar-light shadow-sm text-wrap fs-5">
-    <div class="container-fluid">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="https://github.com/Mwalters75"><i class="bi bi-github" title="GitHub"></i></a>
-        </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://twitter.com/MDWalters124"><i class="bi bi-twitter" title="Twitter"></i></a>
-        </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://deviantart.com/mdwalters"><i class="bi bi-brush-fill" title="DeviantArt"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://imood.com/users/pancake"><i class="bi bi-emoji-smile-fill" title="Imood"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://mdwalterscreations.wordpress.com/author/mdwalters124/"><i class="bi bi-wordpress" title="Blog"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://reddit.com/user/pancakeflipper124/"><i class="bi bi-reddit" title="Reddit"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://youtube.com/channel/UCnAOoxdT4-oCuESfNHK3HXQ"><i class="bi bi-youtube" title="YouTube"></i></a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <Header motd={motd}/>
+  <SocialBar/>
   <div class="main-content">
     <h2>About</h2>
     <p>Hi, I'm M.D.! You may know me as MDWalters, MDWalters124, pancake, or pancake_flipper, but yep, they're all me. Most of the stuff I make are useless, but hey, people actually like them!</p>
