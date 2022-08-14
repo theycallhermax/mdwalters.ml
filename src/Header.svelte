28e0c8aee23adcadcdc17f64b2abfe9c96617d84
M.D. Walters <808426@pdsb.net>
2022-08-14 02:10:47 -0400

    Update Header.svelte

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,13 +1,8 @@
 <script>
   export var motd;
-  export let i;
-  
-  export function easterEgg() {
-      alert("Mr. Header wanted me to tell you to leave him alone")
-  }
 </script>
 
 <div class="container-fluid p-5 bg-primary text-white">
-  <center on:click={easterEgg}><h1 class="display-1">M.D. Walters' Website!</h1></center>
+  <center><h1 class="display-1">M.D. Walters' Website!</h1></center>
   <marquee class="h4">{motd}</marquee>
 </div>
