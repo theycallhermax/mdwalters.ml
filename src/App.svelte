668e838e43948d726cea3f78938e65869e1c234a
M.D. Walters <808426@pdsb.net>
2022-08-14 17:23:09 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -10,9 +10,7 @@
   </Header motd={motd}>
   </SocialBar>
   <div class="main-content">
-    <Container background="#000000" colour="#FFFFFF">
-      <h2>About</h2>
-      <p>Hi, I'm M.D.! You may know me as MDWalters, MDWalters124, pancake, or pancake_flipper, but yep, they're all me. Most of the stuff I make are useless, but hey, people actually like them!</p>
-    </Container>
+    <h2>About</h2>
+    <p>Hi, I'm M.D.! You may know me as MDWalters, MDWalters124, pancake, or pancake_flipper, but yep, they're all me. Most of the stuff I make are useless, but hey, people actually like them!</p>
   </div>
 </main>
