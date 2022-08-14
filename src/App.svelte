5a0f66e974301e4420288864c64d2b27a349a9c2
M.D. Walters <808426@pdsb.net>
2022-08-14 17:17:36 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,18 @@
 <script>
   import Header from './lib/Header.svelte';
   import SocialBar from './lib/SocialBar.svelte';
+  import Container from './lib/Container.svelte';
 
   export var motd = "Finally got Statusflare to work";
 </script>
 
 <main>
-  <Header motd={motd}/>
-  <SocialBar/>
+  </Header motd={motd}>
+  </SocialBar>
   <div class="main-content">
-    <h2>About</h2>
-    <p>Hi, I'm M.D.! You may know me as MDWalters, MDWalters124, pancake, or pancake_flipper, but yep, they're all me. Most of the stuff I make are useless, but hey, people actually like them!</p>
+    <Container background="#000000" colour="#FFFFFF>
+      <h2>About</h2>
+      <p>Hi, I'm M.D.! You may know me as MDWalters, MDWalters124, pancake, or pancake_flipper, but yep, they're all me. Most of the stuff I make are useless, but hey, people actually like them!</p>
+    </Container>
   </div>
 </main>
