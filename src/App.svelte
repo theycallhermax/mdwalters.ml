7e2744dbd7c1a6fd275b7dbdafc9f7d148ab143b
M.D. Walters <808426@pdsb.net>
2022-08-14 23:02:23 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 <main>
   <Header motd={motd}/>
-  </SocialBar>
+  <SocialBar/>
   <div class="main-content">
     <h2>About</h2>
     <p>Hi, I'm M.D.! You may know me as MDWalters, MDWalters124, pancake, or pancake_flipper, but yep, they're all me. Most of the stuff I make are useless, but hey, people actually like them!</p>
