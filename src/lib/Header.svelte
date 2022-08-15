91f900519b420953d9de51854648e24e1c5512fd
M.D. Walters <808426@pdsb.net>
2022-08-14 23:09:04 -0400

    Update Header.svelte

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,5 +4,5 @@
 
 <div class="container-fluid p-5 bg-primary text-white">
   <center><h1 class="display-1">M.D. Walters' Website!</h1></center>
-  <marquee class="h4">{motd}</marquee>
+  <marquee class="h4"><i class="bi bi-lightbulb-fill"></i> {motd}</marquee>
 </div>
