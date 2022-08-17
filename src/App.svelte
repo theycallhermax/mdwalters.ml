6b733440627a7dffddb7800cef53d93e4948f5c9
M.D. Walters <808426@pdsb.net>
2022-08-17 12:09:51 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
 <main>
   <Header motd={motd}/>
   <SocialBar/>
-  <div class="container m-3 bg-warning">
+  <div class="container bg-warning">
     <h2>About</h2>
     <p>Hey, I'm M.D.! I code stuff that goes into the trash burner when I'm finished coding it, but I code anyway.</p>
   </div>
