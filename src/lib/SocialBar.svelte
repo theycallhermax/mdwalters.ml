6c48c8ec0c6eb48b0beb78f29981ae219cc77592
M.D. Walters <808426@pdsb.net>
2022-08-17 16:11:13 -0400

    Update SocialBar.svelte

--- src/lib/SocialBar.svelte
+++ src/lib/SocialBar.svelte
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-sm bg-light navbar-light shadow-sm fs-4">
-   <div class="container-fluid">
+<nav class="navbar navbar-expand-sm fs-4">
+   <div class="container bg-dark bg-gradient shadow rounded">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" href="https://github.com/mdwalters"><i class="bi bi-github" title="GitHub"></i></a>
