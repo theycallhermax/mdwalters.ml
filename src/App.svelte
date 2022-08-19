43e8a8e6714887cf51db0e3fb5a610a0c53f2c7a
M.D. Walters <808426@pdsb.net>
2022-08-19 15:44:20 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-  import Dock from './lib/Dock.svelte';
+  import Header from './lib/Header.svelte';
+  import SocialBar from './lib/SocialBar.svelte';
+
   export var motd = "The Hacker's Keyboard is growing on me";
 </script>
 
 <main>
-  <Dock motd={motd}/>
-  <div class="container">
+  <Header motd={motd}/>
+  <SocialBar/>
+  <div class="container m-3">
     <h2>About</h2>
     <p>Hey, I'm M.D.! I code stuff that goes into the trash burner when I'm finished coding it, but I code anyway.</p>
   </div>
