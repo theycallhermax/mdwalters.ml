edb8afecece48943e17374770d43ae66265a79cb
M.D. Walters <808426@pdsb.net>
2022-08-24 13:20:21 -0400

    Update Content.svelte

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -1,10 +1,10 @@
 <div class="container m-3">
-  <h2>About <small>what you need to know about me</small></h2>
+  <h2>About</h2> <small>what you need to know about me</small>
   <p>Hey, I'm M.D.! I code stuff that goes into the trash burner when I'm finished coding it, but I code anyway.</p>
 </div>
 
 <div class="container m-3">
-  <h2>Projects <small>what i'm currently working on</small></h2>
+  <h2>Projects</h2> <small>what i'm currently working on</small>
   <div class="list-group">
     <li class="list-group-item"><a href="https://github.com/mdwalters/Meower-TUI">Meower TUI</a> <small>a simple, clean meower client</small></li>
   </div>  
