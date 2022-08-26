dccc6573041aa71ba3c2bbfc8d9a565cd522aee8
M.D. Walters <808426@pdsb.net>
2022-08-25 21:27:27 -0400

    Update Content.svelte

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -21,7 +21,7 @@
 <div class="container m-3">
   <h2>Stickers <small>the cool stuff</small></h2>
   <span class="text-wrap">
-    <img src="https://cutercounter.com/hits.php?id=humxfkcnk&nd=6&style=80" border="0" alt="website counter">
+    <img src="https://www.cutercounter.com/hits.php?id=humxfkcnk&nd=6&style=80" border="0" alt="website counter">
     <img src="https://moods.imood.com/display/uname-pancake/imood.gif" alt="The current mood of pancake at www.imood.com" border="0">
   </span>
 </div>
