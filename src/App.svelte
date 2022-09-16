0b9e9197c6fb430dccc6f9c0a0de8772b09f3a55
M.D. Walters <808426@pdsb.net>
2022-09-16 19:20:58 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   import SocialBar from './lib/SocialBar.svelte';
   import Content from './lib/Content.svelte';
 
-  export var motd = "Meower = Cool";
+  export var motd = "As of September 16, 2022, the MOTD was last updated 14 days ago";
 </script>
 
 <main>
