48d2cfc724c3feea112add631b39234f35d0ead4
M.D. Walters <808426@pdsb.net>
2022-09-19 11:37:36 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,11 @@
   import SocialBar from './lib/SocialBar.svelte';
   import Content from './lib/Content.svelte';
 
-  export var motd = ["Never gonna give you up", "Hello world!"];
+  //export var motd = ["Never gonna give you up", "Hello world!"];
 </script>
 
 <main>
-  <Header motd={motd[Math.floor(Math.random() * eightBall.length)]}/>
+  <Header motd={motd}/>
   <SocialBar/>
   <Content/>
 </main>
