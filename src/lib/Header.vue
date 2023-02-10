296a0f0b70dd99e2dee46c2547dfbed436cca08e
M.D. Walters <808426@pdsb.net>
2023-02-10 19:17:15 +0000

    Start Vue port

---
+++ src/lib/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    export let fullscreen: boolean;
+</script>
+
+<template>
+    <div class="container-fluid bg-primary p-5 text-light text-center">
+        <h1 class="display-1">M.D. Walters' Website!</h1>
+        <h2 class="text-wrap hstack gap-2 justify-content-center">
+            <a href="https://blog.mdwalters.ml/" class="link-light text-center"><i class="bi bi-journal-text"></i></a> 
+            <a href="https://blog.mdwalters.ml/feed.xml" class="link-light text-center"><i class="bi bi-rss"></i></a>
+            <a href="https://files.mdwalters.ml/" class="link-light text-center"><i class="bi bi-folder2"></i></a>
+            <a href="https://github.com/mdwalters" class="link-light text-center"><i class="bi bi-github"></i></a>
+            <a href="https://youtube.com/@mdwalters" class="link-light text-center"><i class="bi bi-youtube"></i></a>
+            <a href="mailto:mail@mdwalters.ml" class="link-light text-center"><i class="bi bi-envelope"></i></a>
+            <a href="https://tiktok.com/@mdwaltersthefurry" class="link-light text-center"><i class="bi bi-tiktok"></i></a>
+            <a href="https://reddit.com/u/pancakeflipper124" class="link-light text-center"><i  class="bi bi-reddit"></i></a>
+        </h2>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+    import Header from "../lib/Header.vue";
+</script>
+
+<template>
+    <Header fullscreen={{true}} />
+</template>
